a8d0ae747b4ef0db2286e08d63267cf8267c2f00
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-01-05 16:44:39 +0530

    Remove the firefox submodule
    It's unnecessary. It might never happen. We'll just keep the
    custom_prefs.js file.

---
+++ custom_prefs.js
@@ -0,0 +1,20 @@
+// General prefs
+// Probably to be updated in /etc/firefox/prefs.js
+// pref() or lockPref()
+
+browser.uiCustomization.state - {"placements":{"PanelUI-contents":["social-share-button","feed-button","history-panelmenu","email-link-button","developer-button","preferences-button","add-ons-button","characterencoding-button"],"addon-bar":["addonbar-closebutton","status-bar"],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["urlbar-container","search-container","bookmarks-menu-button","downloads-button","sync-button","loop-button"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"]},"seen":[],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar","PanelUI-contents","addon-bar"],"currentVersion":5,"newElementCount":0}
+
+// Integer
+browser.startup.page - 3
+
+// Strings
+browser.startup.homepage - about:home
+
+browser.search.defaultenginename - DuckDuckGo
+
+services.sync.account - suomynonatsivitca@gmail.com  //test
+
+// Booleans
+privacy.donottrackheader.enabled - true
+
+browser.download.useDownloadDir - false
