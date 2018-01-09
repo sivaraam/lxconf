9d0ded79fa502bfa039e68459056f2830a42c5fa
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-01-09 15:02:11 +0530

    firefox prefs: update prefs
    This removes the prefs that could be handled by Firefox Sync itself
    and updates the UI Customization to meet the changes in the newer
    version.

--- custom_prefs.js
+++ custom_prefs.js
@@ -2,19 +2,17 @@
 // Probably to be updated in /etc/firefox/prefs.js
 // pref() or lockPref()
 
-browser.uiCustomization.state - {"placements":{"PanelUI-contents":["social-share-button","feed-button","history-panelmenu","email-link-button","developer-button","preferences-button","add-ons-button","characterencoding-button"],"addon-bar":["addonbar-closebutton","status-bar"],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["urlbar-container","search-container","bookmarks-menu-button","downloads-button","sync-button","loop-button"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"]},"seen":[],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar","PanelUI-contents","addon-bar"],"currentVersion":5,"newElementCount":0}
-
-// Integer
-browser.startup.page - 3
-
-// Strings
-browser.startup.homepage - about:home
+browser.uiCustomization.state - {"placements":{"widget-overflow-fixed-list":["screenshots_mozilla_org-browser-action","sync-button"],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","developer-button","social-share-button","feed-button","library-button","loop-button","action-button--browsecbrowseccom-browsec-tbb","action-button--testpilot-addon-main","_testpilot-addon-browser-action","_86d73a1c-2ec5-4b7a-b249-60cec805dc99_-browser-action","snoozetabs_mozilla_com-browser-action","https-everywhere-eff_eff_org-browser-action","jid1-mnnxcxisbpnsxq-eff_jetpack-browser-action","_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"],"addon-bar":["addonbar-closebutton","status-bar"],"undefined":["history-panelmenu","developer-button","preferences-button","add-ons-button"]},"seen":["pocket-button","developer-button","action-button--browsecbrowseccom-browsec-tbb","screenshots_mozilla_org-browser-action","webcompat-reporter-button","webide-button","action-button--testpilot-addon-main","_testpilot-addon-browser-action","_86d73a1c-2ec5-4b7a-b249-60cec805dc99_-browser-action","snoozetabs_mozilla_com-browser-action","https-everywhere-eff_eff_org-browser-action","jid1-mnnxcxisbpnsxq-eff_jetpack-browser-action","_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action"],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar","PanelUI-contents","addon-bar","widget-overflow-fixed-list"],"currentVersion":13,"newElementCount":57}
 
 browser.search.defaultenginename - DuckDuckGo
 
-services.sync.account - suomynonatsivitca@gmail.com  //test
+/* Covered by Firefox sync */
+// Integer
+// browser.startup.page - 3
+
+// Strings
+// browser.startup.homepage - about:home
 
 // Booleans
-privacy.donottrackheader.enabled - true
-
-browser.download.useDownloadDir - false
+// privacy.donottrackheader.enabled - true
+// browser.download.useDownloadDir - false
